35792e9a77149752c0c60e36e0b12f8a081ac8ff
scybulski <szymon9@windowslive.com>
2015-01-16 17:59:35 +0100

    Workaround capturing key pressed event, still considering moving to Slick2d

--- BattleCityPUT/src/battlecityput/FXMLDocumentController.java
+++ BattleCityPUT/src/battlecityput/FXMLDocumentController.java
@@ -9,10 +9,19 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.scene.control.TextField;
+
 
 /**
  *
@@ -22,21 +31,42 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
+    @FXML private TextField testTextField;
+    @FXML private AnchorPane battlefield;
+    @FXML private Rectangle field;
+    @FXML private Text levelcounter;
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        battlefield.setVisible(false);
+        field.setVisible(false);
     }
 
+    
     @FXML
     private void handleMainMenuKeyPressedAction(KeyEvent event) {
-        System.out.println("Key pressed!");
+        System.out.print("Key ");
+        System.out.print(event.getCode());
+        System.out.println(" pressed!");
+        if(event.getCode() == KeyCode.ENTER)
+        {
+            battlefield.setVisible(true);
+            testTextField.setonke
+            testTextField.setOnKeyPressed(handleBattleFieldBeforeKeyPressedAction);
+        }
     }
-
     @FXML
-    private void handleMainMenuTankClicked(MouseEvent event) {
-        System.out.println("Tank clicked!");
-
+    private void handleBattleFieldBeforeKeyPressedAction(KeyEvent event) {
+        System.out.print("Key ");
+        System.out.print(event.getCode());
+        System.out.println(" pressed!");
+        if(event.getCode() == KeyCode.UP)
+            levelcounter.setText(Integer.toString(Integer.parseInt(levelcounter.getText()) + 1));
+            
     }
+
+    
+
     
 }
